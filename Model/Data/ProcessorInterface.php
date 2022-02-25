51749d1eb3c1968715198a8977a20c9690b0986a
Anton Starodinov <starodinov@raveinfosys.com>
2022-02-25 20:16:29 +0300

    LSM2-27 Locales mapping: the basic structure Preparations, pt.3. Data processor / operation structure

---
+++ Model/Data/ProcessorInterface.php
@@ -0,0 +1,16 @@
+<?php
+namespace Aheadworks\Langshop\Model\Data;
+
+use Magento\Framework\Exception\LocalizedException;
+
+interface ProcessorInterface
+{
+    /**
+     * Process data
+     *
+     * @param array $data
+     * @return array
+     * @throws LocalizedException
+     */
+    public function process($data);
+}
