f5b40a667f2cc83f3e8a1794fafa37e6bafb0334
Evgeny Levinsky <levinsky@aheadworks.com>
2022-05-22 17:00:12 -0400

    LSM2-114: added a curl sender to unify curl requests

--- Controller/Adminhtml/Saas/Translate.php
+++ Controller/Adminhtml/Saas/Translate.php
@@ -3,29 +3,21 @@ declare(strict_types=1);
 
 namespace Aheadworks\Langshop\Controller\Adminhtml\Saas;
 
+use Aheadworks\Langshop\Model\Saas\CurlSender;
 use Aheadworks\Langshop\Model\Saas\Request\Translate as TranslateRequest;
-use Exception;
 use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
 use Magento\Framework\App\Action\HttpPostActionInterface;
+use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\Controller\ResultInterface;
 use Magento\Framework\Exception\LocalizedException;
-use Magento\Framework\HTTP\Client\CurlFactory;
-use Magento\Framework\Controller\ResultFactory;
-use Magento\Framework\Serialize\SerializerInterface;
-use Psr\Log\LoggerInterface;
 
 class Translate extends Action implements HttpPostActionInterface
 {
     /**
-     * @var CurlFactory
+     * @var CurlSender
      */
-    private CurlFactory $curlFactory;
-
-    /**
-     * @var LoggerInterface
-     */
-    private LoggerInterface $logger;
+    private CurlSender $curlSender;
 
     /**
      * @var TranslateRequest
@@ -33,36 +25,26 @@ class Translate extends Action implements HttpPostActionInterface
     private TranslateRequest $translateRequest;
 
     /**
-     * @var SerializerInterface
-     */
-    private SerializerInterface $serializer;
-
-    /**
      * @param Context $context
-     * @param CurlFactory $curlFactory
-     * @param LoggerInterface $logger
+     * @param CurlSender $curlSender
      * @param TranslateRequest $translateRequest
-     * @param SerializerInterface $serializer
      */
     public function __construct(
         Context $context,
-        CurlFactory $curlFactory,
-        LoggerInterface $logger,
-        TranslateRequest $translateRequest,
-        SerializerInterface $serializer
+        CurlSender $curlSender,
+        TranslateRequest $translateRequest
     ) {
         parent::__construct($context);
 
-        $this->curlFactory = $curlFactory;
-        $this->logger = $logger;
+        $this->curlSender = $curlSender;
         $this->translateRequest = $translateRequest;
-        $this->serializer = $serializer;
     }
 
     /**
      * Sends request to Langshop to translate particular resource
      *
      * @return ResultInterface
+     * @throws LocalizedException
      */
     public function execute()
     {
@@ -71,20 +53,12 @@ class Translate extends Action implements HttpPostActionInterface
             'message' => __('Translation started. Please wait.')
         ];
 
-        $curl = $this->curlFactory->create();
+        $curl = $this->curlSender->post(
+            $this->translateRequest->getUrl(),
+            $this->getRequestParams()
+        );
 
-        try {
-            $curl->post(
-                $this->translateRequest->getUrl(),
-                $this->serializer->serialize($this->getRequestParams())
-            );
-
-            if ($curl->getStatus() !== 200) {
-                throw new Exception();
-            }
-        } catch (Exception $exception) {
-            $this->logger->error($exception);
-
+        if ($curl->getStatus() !== 200) {
             $result = [
                 'error' => true,
                 'message' => __('A technical problem with the server created an error.')
