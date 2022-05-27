33d16b7e39e54584a8762c220531bef5d3caadc7
vys0tsky <vysotsky@aheadworks.com>
2022-05-27 16:34:15 +0300

    LSM2-144 - /installation/confirm response

--- Api/SaasManagementInterface.php
+++ Api/SaasManagementInterface.php
@@ -3,13 +3,15 @@ declare(strict_types=1);
 
 namespace Aheadworks\Langshop\Api;
 
+use Aheadworks\Langshop\Api\Data\Saas\ConfirmationResultInterface;
+
 interface SaasManagementInterface
 {
     /**
      * Save key
      *
      * @param string $publicKey
-     * @return bool
+     * @return \Aheadworks\Langshop\Api\Data\Saas\ConfirmationResultInterface
      */
-    public function saveKey(string $publicKey): bool;
+    public function saveKey(string $publicKey): ConfirmationResultInterface;
 }
