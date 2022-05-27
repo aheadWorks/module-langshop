9ea95a31dd906fe13f5f0f68e3bb05d4595a054a
Evgeny Levinsky <levinsky@aheadworks.com>
2022-05-27 17:05:53 -0400

    LSM2-143: added locale name to the list of stores

--- Model/Source/ListToTranslate.php
+++ Model/Source/ListToTranslate.php
@@ -5,6 +5,7 @@ namespace Aheadworks\Langshop\Model\Source;
 
 use Aheadworks\Langshop\Model\Config\Locale as LocaleConfig;
 use Magento\Framework\Data\OptionSourceInterface;
+use Magento\Framework\Locale\ListsInterface as LocaleListsInterface;
 use Magento\Framework\Locale\ResolverInterface as LocaleResolverInterface;
 use Magento\Store\Model\System\Store as StoreOptions;
 
@@ -21,6 +22,11 @@ class ListToTranslate implements OptionSourceInterface
     private LocaleConfig $localeConfig;
 
     /**
+     * @var LocaleListsInterface
+     */
+    private LocaleListsInterface $localeLists;
+
+    /**
      * @var LocaleResolverInterface
      */
     private LocaleResolverInterface $localeResolver;
@@ -28,15 +34,18 @@ class ListToTranslate implements OptionSourceInterface
     /**
      * @param StoreOptions $storeOptions
      * @param LocaleConfig $localeConfig
+     * @param LocaleListsInterface $localeLists
      * @param LocaleResolverInterface $localeResolver
      */
     public function __construct(
         StoreOptions $storeOptions,
         LocaleConfig $localeConfig,
+        LocaleListsInterface $localeLists,
         LocaleResolverInterface $localeResolver
     ) {
         $this->storeOptions = $storeOptions;
         $this->localeConfig = $localeConfig;
+        $this->localeLists = $localeLists;
         $this->localeResolver = $localeResolver;
     }
 
@@ -47,30 +56,49 @@ class ListToTranslate implements OptionSourceInterface
      */
     public function toOptionArray()
     {
-        return $this->disableOptions(
+        return $this->prepareOptions(
             $this->storeOptions->toOptionArray(),
             $this->localeResolver->getDefaultLocale()
         );
     }
 
     /**
-     * Disables options-stores with default locale
+     * Prepares options-stores with default locale
      *
      * @param array $options
      * @param string $defaultLocale
      * @return array
      */
-    private function disableOptions(array $options, string $defaultLocale): array
+    private function prepareOptions(array $options, string $defaultLocale): array
     {
         foreach ($options as &$option) {
             if (is_array($option['value'])) {
-                $option['value'] = $this->disableOptions($option['value'], $defaultLocale);
+                $option['value'] = $this->prepareOptions($option['value'], $defaultLocale);
             } else {
                 $locale = $this->localeConfig->getValue((int) $option['value']);
+
+                $option['label'] = sprintf('%s - %s', $option['label'], $this->getLocaleName($locale));
                 $option['disabled'] = $locale === $defaultLocale;
             }
         }
 
         return $options;
+    }
+
+    /**
+     * Retrieves locale name by its code
+     *
+     * @param string $locale
+     * @return string
+     */
+    private function getLocaleName(string $locale): string
+    {
+        foreach ($this->localeLists->getOptionLocales() as $localeOption) {
+            if ($localeOption['value'] === $locale) {
+                return $localeOption['label'];
+            }
+        }
+
+        return '';
     }
 }
