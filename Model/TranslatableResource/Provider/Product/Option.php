00f80687e68880a3950982a80dc683959b49fd66
Evgeny Levinsky <levinsky@aheadworks.com>
2022-06-03 14:26:38 -0400

    LSM2-111: getting localized options for product collection

---
+++ Model/TranslatableResource/Provider/Product/Option.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace Aheadworks\Langshop\Model\TranslatableResource\Provider\Product;
+
+use Magento\Catalog\Model\Product\Option as ProductOption;
+use Magento\Catalog\Model\ResourceModel\Product\Option\CollectionFactory as OptionCollectionFactory;
+use Magento\Store\Model\Store;
+
+class Option
+{
+    /**
+     * @var OptionCollectionFactory
+     */
+    private OptionCollectionFactory $optionCollectionFactory;
+
+    /**
+     * @param OptionCollectionFactory $optionCollectionFactory
+     */
+    public function __construct(
+        OptionCollectionFactory $optionCollectionFactory
+    ) {
+        $this->optionCollectionFactory = $optionCollectionFactory;
+    }
+
+    /**
+     * Retrieves product options by product ids
+     *
+     * @param int[] $productIds
+     * @param int|null $storeId
+     * @return ProductOption[]
+     */
+    public function get(array $productIds, ?int $storeId = Store::DEFAULT_STORE_ID): array
+    {
+        $optionCollection = $this->optionCollectionFactory->create()
+            ->addProductToFilter($productIds)
+            ->addTitleToResult($storeId);
+
+        /** @var ProductOption[] $options */
+        $options = $optionCollection->getItems();
+
+        return $options;
+    }
+}
