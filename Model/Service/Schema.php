d6f857758abf072e4642672d37c011cadf9b7556
vys0tsky <vysotsky@aheadworks.com>
2022-06-09 11:03:16 +0300

    LSM2-161 - Create CSV Schema

--- Model/Service/Schema.php
+++ Model/Service/Schema.php
@@ -7,7 +7,12 @@ use Aheadworks\Langshop\Api\Data\SchemaInterface;
 use Aheadworks\Langshop\Api\Data\SchemaInterfaceFactory;
 use Aheadworks\Langshop\Api\SchemaManagementInterface;
 use Aheadworks\Langshop\Model\Schema\ProcessorInterface;
+use Magento\Framework\Encryption\Encryptor;
 use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\File\Csv;
+use Magento\Framework\Module\Dir\Reader as DirReader;
+use Magento\Framework\Module\FullModuleList;
+use Magento\Framework\Translate;
 use Magento\Framework\Webapi\Exception as WebapiException;
 use Psr\Log\LoggerInterface;
 
@@ -19,6 +24,21 @@ class Schema implements SchemaManagementInterface
     private ProcessorInterface $processor;
 
     /**
+     * @var Csv
+     */
+    private Csv $csv;
+
+    /**
+     * @var DirReader
+     */
+    private DirReader $dirReader;
+
+    /**
+     * @var Encryptor
+     */
+    private Encryptor $encryptor;
+
+    /**
      * @var SchemaInterfaceFactory
      */
     private SchemaInterfaceFactory $schemaFactory;
@@ -28,18 +48,36 @@ class Schema implements SchemaManagementInterface
      */
     private LoggerInterface $logger;
 
+    private Translate $translate;
+
+    private FullModuleList $moduleList;
+
     /**
      * @param ProcessorInterface $processor
      * @param SchemaInterfaceFactory $schemaFactory
+     * @param Csv $csv
+     * @param DirReader $dirReader
+     * @param Encryptor $encryptor
+     * @param Translate $translate
      * @param LoggerInterface $logger
      */
     public function __construct(
         ProcessorInterface $processor,
         SchemaInterfaceFactory $schemaFactory,
+        Csv $csv,
+        DirReader $dirReader,
+        Encryptor $encryptor,
+        Translate $translate,
+        FullModuleList $moduleList,
         LoggerInterface $logger
     ) {
         $this->processor = $processor;
         $this->schemaFactory = $schemaFactory;
+        $this->csv = $csv;
+        $this->encryptor = $encryptor;
+        $this->dirReader = $dirReader;
+        $this->translate = $translate;
+        $this->moduleList = $moduleList;
         $this->logger = $logger;
     }
 
