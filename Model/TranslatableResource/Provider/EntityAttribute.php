b248945748c8bb7109fb12e5ee0e4e04d2808e54
vys0tsky <vysotsky@aheadworks.com>
2022-06-15 11:25:51 +0300

    LSM2-168 - fix phpstan test

--- Model/TranslatableResource/Provider/EntityAttribute.php
+++ Model/TranslatableResource/Provider/EntityAttribute.php
@@ -16,7 +16,7 @@ class EntityAttribute
     private EntityPool $entityPool;
 
     /**
-     * @var array<int, array<string, Field>>
+     * @var  array<string, array<int, array<string, Field>>>
      */
     private array $attributes;
 
