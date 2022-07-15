6ea87e29c1ddd550a8037ada035ef32b68a03405
vys0tsky <vysotsky@aheadworks.com>
2022-07-15 10:42:15 +0300

    LSM2-221 - fix product options plugin

--- Plugin/Langshop/Model/ResourceModel/TranslatableResource/Product/Option/Save.php
+++ Plugin/Langshop/Model/ResourceModel/TranslatableResource/Product/Option/Save.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 namespace Aheadworks\Langshop\Plugin\Langshop\Model\ResourceModel\TranslatableResource\Product\Option;
 
 use Aheadworks\Langshop\Model\ResourceModel\TranslatableResource\Product as ProductResource;
+use Aheadworks\Langshop\Model\TranslatableResource\Validation\Product\Option as OptionValidation;
 use Magento\Catalog\Model\Product;
 use Magento\Catalog\Model\Product\Option;
 use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\Exception\NoSuchEntityException;
 
 class Save
 {
@@ -16,16 +18,24 @@ class Save
     private const KEY_OPTIONS = 'options';
 
     /**
+     * @var OptionValidation
+     */
+    private OptionValidation $optionValidation;
+
+    /**
      * @var ResourceConnection
      */
     private ResourceConnection $resourceConnection;
 
     /**
+     * @param OptionValidation $optionValidation
      * @param ResourceConnection $resourceConnection
      */
     public function __construct(
+        OptionValidation $optionValidation,
         ResourceConnection $resourceConnection
     ) {
+        $this->optionValidation = $optionValidation;
         $this->resourceConnection = $resourceConnection;
     }
 
@@ -35,11 +45,24 @@ class Save
      * @param ProductResource $productResource
      * @param Product $product
      * @return Product[]
+     * @throws NoSuchEntityException
      */
     public function beforeSave(
         ProductResource $productResource,
         Product $product
     ): array {
+        $options = $product->getData(self::KEY_OPTIONS);
+        if (is_array($options) && $options) {
+            $option = reset($options);
+            if ($option instanceof Option) {
+                $product->setData(self::KEY_OPTIONS, null);
+            } else {
+                foreach ($options as $optionId => $title) {
+                    $this->optionValidation->validate((int) $optionId, (int) $product->getId());
+                }
+            }
+        }
+
         return [
             // @see \Magento\Catalog\Model\Product\Option\SaveHandler
             $product->setData('options_saved', true)
@@ -62,12 +85,11 @@ class Save
         $options = $product->getData(self::KEY_OPTIONS);
         if (is_array($options) && $options) {
             $toInsert = [];
-            /** @var Option $option */
-            foreach ($options as $option) {
+            foreach ($options as $optionId => $title) {
                 $toInsert[] = [
-                    'option_id' => $option->getOptionId(),
+                    'option_id' => $optionId,
                     'store_id' => $product->getStoreId(),
-                    'title' => $option->getTitle()
+                    'title' => $title
                 ];
             }
 
