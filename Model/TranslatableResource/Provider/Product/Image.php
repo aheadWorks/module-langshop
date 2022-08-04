f51c968099d99521edd9d082140225962e877a3d
vys0tsky <vysotsky@aheadworks.com>
2022-08-04 17:00:09 +0300

    LSM2-271 - Incorrect image labels are shown for products

---
+++ Model/TranslatableResource/Provider/Product/Image.php
@@ -0,0 +1,92 @@
+<?php
+declare(strict_types=1);
+namespace Aheadworks\Langshop\Model\TranslatableResource\Provider\Product;
+
+use Magento\Framework\App\ResourceConnection;
+use Magento\Store\Model\Store;
+
+class Image
+{
+    /**
+     * @var ResourceConnection
+     */
+    private ResourceConnection $resourceConnection;
+
+    /**
+     * @var string[]
+     */
+    private array $attributeCodes = [
+        'image',
+        'small_image',
+        'thumbnail'
+    ];
+
+    /**
+     * @param ResourceConnection $resourceConnection
+     */
+    public function __construct(
+        ResourceConnection $resourceConnection
+    ) {
+        $this->resourceConnection = $resourceConnection;
+    }
+
+    /**
+     * Get images
+     *
+     * @param int[] $productIds
+     * @param int $storeId
+     * @return array
+     */
+    public function getImages(array $productIds, int $storeId = Store::DEFAULT_STORE_ID): array
+    {
+        $connection = $this->resourceConnection->getConnection();
+
+        $select = $connection->select()
+            ->from(
+                ['main' => $this->resourceConnection->getTableName('catalog_product_entity_varchar')]
+            )->joinLeft(
+                ['eava' => $this->resourceConnection->getTableName('eav_attribute')],
+                'main.attribute_id = eava.attribute_id',
+                ['attribute_code']
+            )->joinLeft(
+                ['mg' => $this->resourceConnection->getTableName('catalog_product_entity_media_gallery')],
+                'main.value = mg.value',
+                ['value_id']
+            )->joinLeft(
+                ['mgv' => $this->resourceConnection->getTableName('catalog_product_entity_media_gallery_value')],
+                'mg.value_id = mgv.value_id AND main.store_id = mgv.store_id',
+                ['label']
+            )->where(
+                'eava.attribute_code IN (?)', $this->attributeCodes
+            )->where(
+                'mgv.store_id = ?', $storeId
+            )->where(
+                'main.entity_id IN (?)', $productIds
+            );
+
+        return $connection->fetchAssoc($select);
+    }
+
+    /**
+     * Get media gallery values
+     *
+     * @param array $productIds
+     * @param int $storeId
+     * @return array
+     */
+    public function getMediaGalleryValues(array $productIds, int $storeId = Store::DEFAULT_STORE_ID): array
+    {
+        $connection = $this->resourceConnection->getConnection();
+
+        $select = $connection->select()
+            ->from(
+                ['main' => $this->resourceConnection->getTableName('catalog_product_entity_media_gallery_value')]
+            )->where(
+                'main.store_id = ?', $storeId
+            )->where(
+                'main.entity_id IN (?)', $productIds
+            );
+
+        return $connection->fetchAssoc($select);
+    }
+}
---
+++ Plugin/Langshop/Model/ResourceModel/TranslatableResource/Product/Image/Label/Read.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+namespace Aheadworks\Langshop\Plugin\Langshop\Model\ResourceModel\TranslatableResource\Product\Image\Label;
+
+use Aheadworks\Langshop\Model\ResourceModel\TranslatableResource\Product\Collection as ProductCollection;
+use Aheadworks\Langshop\Model\TranslatableResource\Provider\Product\Image as ProductImageProvider;
+use Magento\Catalog\Model\Product;
+
+class Read
+{
+    private ProductImageProvider $imageProvider;
+
+    /**
+     * @param ProductImageProvider $imageProvider
+     */
+    public function __construct(
+        ProductImageProvider $imageProvider
+    ) {
+        $this->imageProvider = $imageProvider;
+    }
+
+    /**
+     * Retrieves image labels for the products
+     *
+     * @param ProductCollection $productCollection
+     * @param Product[] $products
+     * @return Product[]
+     */
+    public function afterGetItems(
+        ProductCollection $productCollection,
+        array $products
+    ): array {
+        if ($products) {
+            $images = $this->imageProvider->getImages(
+                array_keys($products),
+                $productCollection->getStoreId()
+            );
+
+            foreach ($images as $image) {
+                $products[$image['entity_id']]->setData(
+                    $image['attribute_code'] . '_label',
+                    $image['label']
+                );
+            }
+        }
+
+        return $products;
+    }
+}
