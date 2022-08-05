fb07dc57d854dbdb532ed3fad53cc634ada833eb
vys0tsky <vysotsky@aheadworks.com>
2022-08-05 17:03:54 +0300

    LSM2-293 - fix error

--- Model/ResourceModel/TranslatableResource/Csv/Collection.php
+++ Model/ResourceModel/TranslatableResource/Csv/Collection.php
@@ -216,7 +216,7 @@ class Collection extends DataCollection implements CollectionInterface
         $result = [];
         foreach ($csvData as $data) {
             $originalString = $data[CsvFile::ORIGINAL_INDEX];
-            if (count_chars($originalString) < 256) {
+            if (strlen($originalString) < 256) {
                 $result[] = $originalString;
             }
         }
