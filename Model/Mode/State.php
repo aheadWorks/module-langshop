b8a2e6aa8f4901f633f8e650912e589abb0fea4d
Alexey Horoneko <horoneko@aheadworks.com>
2024-12-13 18:56:18 +0300

    https://ravedigital.teamwork.com/app/tasks/36681494 - Added console command to set mode

---
+++ Model/Mode/State.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+
+namespace Aheadworks\Langshop\Model\Mode;
+
+use Exception;
+use Magento\Framework\Exception\LocalizedException;
+
+class State
+{
+    public const APP_BUILDER = 'app_builder';
+    public const LANG_SHOP_APP = 'lang_shop_app';
+
+    /**
+     * @param Flag $flag
+     */
+    public function __construct(private readonly Flag $flag)
+    {
+    }
+
+    /**
+     * Set mode
+     *
+     * @throws Exception
+     */
+    public function setMode(string $mode): void
+    {
+        if (!in_array($mode, [self::APP_BUILDER, self::LANG_SHOP_APP])) {
+            throw new LocalizedException(__('Invalid mode'));
+        }
+        $this->flag
+            ->loadSelf()
+            ->setFlagData($mode)
+            ->save();
+    }
+
+    /**
+     * Get mode
+     *
+     * @return string
+     */
+    public function getMode(): string
+    {
+        try {
+            $flagValue = $this->flag
+                ->loadSelf()
+                ->getFlagData();
+            $mode = $flagValue ?: self::LANG_SHOP_APP;
+        } catch (LocalizedException) {
+            $mode = self::LANG_SHOP_APP;
+        }
+
+        return $mode;
+    }
+
+    /**
+     * Check if mode is app builder
+     *
+     * @return bool
+     */
+    public function isAppBuilderMode(): bool
+    {
+        return $this->getMode() === self::APP_BUILDER;
+    }
+}
